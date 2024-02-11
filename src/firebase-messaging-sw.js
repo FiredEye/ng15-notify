2fba228f979f604d1e590acf9f153698c5632a24
Rashik Tuladhar <rashiktuladhar13@gmail.com>
2024-02-11 13:57:39 +0545

    ..

--- src/firebase-messaging-sw.js
+++ src/firebase-messaging-sw.js
@@ -21,16 +21,21 @@ fetch('config/config.json')
         messaging = firebase.messaging();
         messaging.setBackgroundMessageHandler(async function (payload) {
           console.log('[firebase-messaging-sw.js] Received background message ', payload);
-          // Customize notification here
-          let notification = payload;
-          let title = notification.data.title || '';
-          let message = notification.data.message || '';
-          const notificationTitle = title;
-          const notificationOptions = {
-              body: message,
-              icon: 'assets/icon.png',
-              data: payload.data,
-          };
+        //  Works for android and windows
+        //   let notification = payload;
+        //   let title = notification.data.title || '';
+        //   let message = notification.data.message || '';
+        //   const notificationTitle = title;
+        //   const notificationOptions = {
+        //       body: message,
+        //       icon: 'assets/icon.png',
+        //       data: payload.data,
+        //   };
+        const notificationTitle = payload.notification.title;
+        const notificationOptions = {
+          body: payload.notification.body,
+          icon: payload.notification.image,
+        };
   
           return self.registration.showNotification(notificationTitle,
               notificationOptions);     
@@ -39,36 +44,36 @@ fetch('config/config.json')
     });
 
 
-self.addEventListener('notificationclick', function(event) {
-    event.notification.close(); // Android needs explicit close.
-    if(event.notification.data) {
-        event.notification.data.isFirebaseMessaging = false;
-    }
-    event.waitUntil(
-        clients.matchAll({includeUncontrolled: true,type: 'window'}).then( windowClients => {
+// self.addEventListener('notificationclick', function(event) {
+//     event.notification.close(); // Android needs explicit close.
+//     if(event.notification.data) {
+//         event.notification.data.isFirebaseMessaging = false;
+//     }
+//     event.waitUntil(
+//         clients.matchAll({includeUncontrolled: true,type: 'window'}).then( windowClients => {
         
-        let rootUrl = new URL('/', location).href;
-            // Check if there is already a window/tab open with the target URL
-            for (var i = 0; i < windowClients.length; i++) {
-                var client = windowClients[i];
-                var url = client.url;
-                // If so, just focus it.
-                if (url.indexOf(rootUrl) == 0  && 'focus' in client) {
-                    client.postMessage(event.notification.data);
-                    return client.focus();
-                }
-            }
-            // If not, then open the target URL in a new window/tab.
-            if (clients.openWindow) {
-                return clients.openWindow(url);
-            }
-        })
-    );
-});
+//         let rootUrl = new URL('/', location).href;
+//             // Check if there is already a window/tab open with the target URL
+//             for (var i = 0; i < windowClients.length; i++) {
+//                 var client = windowClients[i];
+//                 var url = client.url;
+//                 // If so, just focus it.
+//                 if (url.indexOf(rootUrl) == 0  && 'focus' in client) {
+//                     client.postMessage(event.notification.data);
+//                     return client.focus();
+//                 }
+//             }
+//             // If not, then open the target URL in a new window/tab.
+//             if (clients.openWindow) {
+//                 return clients.openWindow(url);
+//             }
+//         })
+//     );
+// });
 
-self.addEventListener('message', function(event){
-});
+// self.addEventListener('message', function(event){
+// });
 
-self.addEventListener('install', function(event) {
-    self.skipWaiting();
-});
+// self.addEventListener('install', function(event) {
+//     self.skipWaiting();
+// });
