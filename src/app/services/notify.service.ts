0d429da762f25acd04fe29ec474b54ec2449f26d
Rashik Tuladhar <rashiktuladhar13@gmail.com>
2024-02-11 17:27:27 +0545

    upd notify

--- src/app/services/notify.service.ts
+++ src/app/services/notify.service.ts
@@ -29,7 +29,7 @@ export class NotifyService {
   }
   async requestAndSendToken(){
     const permission = await Notification.requestPermission();
-    if (localStorage['token']) {
+    if (localStorage['token']!==null) {
       alert("Token already exist");
     }
     else if (permission === 'granted') {
